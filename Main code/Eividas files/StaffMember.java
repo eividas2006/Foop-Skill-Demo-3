4e037a0b2830c64c3bc391908a3ad6a71a77e4db
eividas2006 <2024ezavedskas@loetbstudent.ie>
2026-04-16 20:48:27 +0100

    Update StaffMember.java
    commented it out

--- Main code/Eividas files/StaffMember.java
+++ Main code/Eividas files/StaffMember.java
@@ -1,27 +1,32 @@
-package com.mycompany.memberlibrary;
+package com.mycompany.memberlibrary; //package
 
-public class StaffMember extends Member {
+//staff class extends Member
+public class StaffMember extends Member {//start class
 
+    //max books is 10
     private int maxBooks = 10;
 
+    //constructor
     public StaffMember(int memberID, String name, String email, int borrowedBooks) {
         super(memberID, name, email, borrowedBooks);
         // constructor implementation
     }
 
+    //borrow limit
     @Override
     public int calculateBorrowLimit() {
         return maxBooks;
     }
 
-
+    //sets max books
     public void setMaxBooks(int maxBooks) {
         this.maxBooks = maxBooks;
     }
 
+    //staff information
     @Override
     public String toString() {
         // toString implementation
         return null;
     }
-}
+}//end class

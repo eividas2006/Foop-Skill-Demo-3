02f3f3c8096eca90483d746b896c61a5d842ff1c
eividas2006 <2024ezavedskas@loetbstudent.ie>
2026-04-16 20:51:12 +0100

    Update StudentMember.java
    commented it out

--- Main code/Eividas files/StudentMember.java
+++ Main code/Eividas files/StudentMember.java
@@ -1,29 +1,34 @@
 
-package com.mycompany.memberlibrary;
+package com.mycompany.memberlibrary; //package
 
 // StudentMember.java
-public class StudentMember extends Member {
 
+// Student class extends Member
+public class StudentMember extends Member { //start class
+
+    //max books is 5
     private int maxBooks = 5;
 
+    //constructor
     public StudentMember(int memberID, String name, String email, int borrowedBooks) {
         super(memberID, name, email, borrowedBooks);
     }
 
-       
-    
+    //borrow limit
     @Override
     public int calculateBorrowLimit() {
         return maxBooks;
     }
-
+    
+    //sets max books
     public void setMaxBooks(int maxBooks) {
         this.maxBooks = maxBooks;
     }
 
+    //student details
     @Override
     public String toString() {
         return super.toString()
         + "Max Books: " + maxBooks;
     }
-}
+}//end class
